38f08c568fabdea657ee047855e32d3d9acfcb54
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2016-03-30 18:03:16 -0500

    Introduce main game loop

--- src/script.js
+++ src/script.js
@@ -7,19 +7,43 @@
  *   7 8 9
  */
 
-$(document).ready(function() {
-  
+$(document).ready(init);
+
+
+function init() {
   // insert byline
   $('h1').after( $('<hr />') ).after( $('<i>').text('by Nicholas Neumann-Chun') );
+
+  // insert turn indicator
+  $('.board').after( $('<h2>').addClass('turn-indicator') );
+
+  // insert restart button
+  $('.turn-indicator').after( $('<button>').text('Restart!').addClass('hidden') );
+  $('button').click(startGame);
 
   // label buttons
   $('.btn').each(function(i) {
     $(this).data('id', i + 1);
   });
 
-  // click handler for buttons
-  $('.btn').click(function() {
-    console.log('button clicked:', $(this).data('id'));
-  });
+  startGame();
+};
 
-});
+
+function startGame() {
+  var whoseTurn = 'X';
+
+  // initialize board
+  $('.btn').val(' ');
+  $('.turn-indicator').text("X's turn");
+
+  // handle button clicks
+  $('.btn').on('click', function() {
+    console.log('button clicked:', $(this).data('id'));
+
+    $(this).val(whoseTurn);
+
+    whoseTurn = whoseTurn === 'X' ? 'O' : 'X';
+    $('.turn-indicator').text(whoseTurn + "'s turn");
+  });
+}
